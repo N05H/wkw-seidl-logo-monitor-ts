a2459006cd1dac4288fadc9288cc8767dde14cf7
Christopher Schmidt <lc.schmidt91@gmail.com>
2025-02-08 19:55:11 +0100

    feat: added telegram client feat: added .env for config and chatids beign loaded but ignored

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 
-
+import dotenv from 'dotenv';    
 import cron from "node-cron";
 import logger from "./modules/logger";
+import TelegramClient from './modules/telegramclient'
+import path from 'path';
+
+
+// Load environment variables from .env file
+dotenv.config();
+const token = process.env.TELEGRAM_TOKEN || "";
+const chatIdFile = path.join(__dirname, 'data/chatIds.json');
+const telegramClient = new TelegramClient(token, chatIdFile);
+
 
 logger.info("Application started")
+
+setInterval(() => console.log("Alive"), 3000)
---
+++ src/modules/telegramclient.ts
@@ -0,0 +1,94 @@
+
+import { Telegraf } from 'telegraf';
+import { EventEmitter } from 'events';
+import fs from 'fs';
+import logger from "./logger";
+
+
+class TelegramClient extends EventEmitter {
+    private token: string;
+    private chatIdFile: string;
+    private chatIds: number[];
+    private bot: Telegraf;
+
+    constructor(token: string, chatIdFile: string) {
+        super();
+        this.token = token;
+        this.chatIdFile = chatIdFile;
+        this.chatIds = this.readChatIds(this.chatIdFile);
+        this.bot = new Telegraf(this.token);
+        this.initializeHandles();
+
+        this.bot.launch();
+
+        process.once('SIGINT', () => this.bot.stop('SIGINT'));
+        process.once('SIGTERM', () => this.bot.stop('SIGTERM'));
+    }
+
+
+
+    private initializeHandles(): void {
+        // Middleware to handle new chat IDs
+        this.bot.use((ctx, next) => {
+            const chatId = ctx.chat?.id;
+            if (chatId === undefined) {
+                logger.warn('Received a message without a chat ID');
+                return next(); // Optionally, you can stop further processing here or return an error
+            }
+
+            if (!this.chatIds.includes(chatId)) {
+                this.chatIds.push(chatId);
+                this.writeChatIds(this.chatIdFile, this.chatIds);
+            }
+            return next();
+        });
+
+        // Simple start message
+        this.bot.start((ctx) => ctx.reply('Welcome to the bot!'));
+
+        // Fallback message for all other cases
+        this.bot.hears(/.*/, (ctx) => ctx.reply('Hello, send a message!'));
+    }
+
+
+    private readChatIds(path: string): number[] {
+        if (fs.existsSync(path)) {
+            const data = fs.readFileSync(path, 'utf-8');
+            try {
+                return JSON.parse(data);
+            } catch (error) {
+                logger.error(`Error reading chat IDs: ${error}`);
+                return [];
+            }
+        }
+        return [];
+    }
+
+
+    private writeChatIds(path: string, chatIds: number[]): void {
+        try {
+            fs.writeFileSync(path, JSON.stringify(chatIds, null, 2), 'utf-8');
+        } catch (error) {
+            logger.error(`Error writing chat IDs: ${error}`);
+        }
+    }
+
+
+    public sendMessage(msg: string): void {
+        this.chatIds.forEach((id) => {
+            this.bot.telegram
+                .sendMessage(id, msg)
+                .then(() => {
+                    logger.info(`Sent message to chat ID: ${id}`);
+                })
+                .catch((error) => {
+                    logger.error(`Failed to send message to chat ID: ${id}. Error: ${error}`);
+                });
+        });
+    }
+
+}
+
+
+
+export default TelegramClient;
